aed39c046db53d9ed0bd976d5cd403009bc01349
daria.ch.prozorro <daria.ch.prozorro@gmail.com>
2022-09-02 23:37:38 +0300

    hw_6_new_1

--- src/piccastore/main_24.py
+++ src/piccastore/main_24.py
@@ -2,19 +2,19 @@ import random
 from unittest import case
 from collections import namedtuple
 
+Pizza = namedtuple('Pizza', ['idx', 'name', 'price', 'description'])
 pizzas = [
-    (1, 'Hawaiian', 100, 'Chicken + pineapple + bakery + mozzarella + sauce'),
-    (2, 'Carbonara', 110, 'Bacon + tavern + bakery + marinated + olives + mozzarella'),
-    (3, 'M`yasna', 120, 'Bacon + salami + tomato + mozzarella'),
-    (4, 'Margarita', 130, 'Tomato + mozzarella + sauce + salami'),
-    (5, 'Mislivska', 140, 'Hunting sausages + salami + salted cucumber + garlic'),
-    (6, 'Vegetable teriyaki', 150, 'Tomato + mushrooms + bell pepper + pickles'),
-    (7, 'Pepperoni', 160, 'Pepperoni + italian herbs + mozzarella + signature sauce'),
-    (8, 'Francesca', 170, 'Bacon + salami + mushrooms + tomato + olives + mozzarella'),
-    (9, 'Four cheeses', 180, 'Dor bleu cheese + gouda cheese + cream cheese + mozzarella'),
-    (10, 'Sharp', 200, 'Salami + tomatoes + bell pepper + green beans + sauce')
+    Pizza(1, 'Hawaiian', 100, 'Chicken + pineapple + bakery + mozzarella + sauce'),
+    Pizza(2, 'Carbonara', 110, 'Bacon + tavern + bakery + marinated + olives + mozzarella'),
+    Pizza(3, 'M`yasna', 120, 'Bacon + salami + tomato + mozzarella'),
+    Pizza(4, 'Margarita', 130, 'Tomato + mozzarella + sauce + salami'),
+    Pizza(5, 'Mislivska', 140, 'Hunting sausages + salami + salted cucumber + garlic'),
+    Pizza(6, 'Vegetable teriyaki', 150, 'Tomato + mushrooms + bell pepper + pickles'),
+    Pizza(7, 'Pepperoni', 160, 'Pepperoni + italian herbs + mozzarella + signature sauce'),
+    Pizza(8, 'Francesca', 170, 'Bacon + salami + mushrooms + tomato + olives + mozzarella'),
+    Pizza(9, 'Four cheeses', 180, 'Dor bleu cheese + gouda cheese + cream cheese + mozzarella'),
+    Pizza(10, 'Sharp', 200, 'Salami + tomatoes + bell pepper + green beans + sauce')
 ]
-
 while True:
     print('Hello. 0 - Exit, 1 - print all pizzas')
     c = input('Your choise: ')
@@ -22,25 +22,27 @@ while True:
         case '0':
             break
         case '1':
-            for item in pizzas:
-                print(item)
+            for Pizza in pizzas:
+                print(Pizza)
         case '2':
             order = random.randint(1, 6)
             print(order)
         case '3':
             chek_1 = random.sample(pizzas, order)
             print(f"chek_1:{chek_1}")
-            for item in chek_1:
-                print(item, random.randint(1, 3))
+            for Pizza in chek_1:
+                print(Pizza, random.randint(1, 3))
         case '4':
-            pizzas_1 = [item for item in pizzas if item[2] < 150]
+            pizzas_1 = [Pizza for Pizza in pizzas if Pizza.price < 150]
             print(f'the price of pizza is less than 150 UAH:{pizzas_1}')
-            pizzas_2 = [item for item in pizzas if item[2] > 150]
+            pizzas_2 = [Pizza for Pizza in pizzas if Pizza.price > 150]
             print(f'the price of pizza is more than 150 UAH:{pizzas_2}')
-            pizzas_3 = [item for item in pizzas if item[2] == 150]
+            pizzas_3 = [Pizza for Pizza in pizzas if Pizza.price == 150]
             print(f'the price of pizza is 150 UAH:{pizzas_3}')
         case _:
             print('Wrong choise! Try again')
 
-print('Bay')
+print('Bay') #Трішки щось не дуже гарно виходить к консолі(вид) та якщо можна ще допрацюю,
+# почитаю завтра всі документи що Ви скинули
+#захотілося відразу після заняття зробити домашку, перевірити себе як я зрозуміла лекцію)
 
