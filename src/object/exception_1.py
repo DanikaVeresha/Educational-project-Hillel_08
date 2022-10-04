9898e20d2a0fcd9c06d3e06cd1e1fd32afadb3fe
daria.ch.prozorro <daria.ch.prozorro@gmail.com>
2022-10-04 16:52:44 +0300

    hw_11/2

---
+++ src/object/exception_1.py
@@ -0,0 +1,16 @@
+from object.my_exception import MyException
+
+
+try:
+    price = int(input('price: '))
+    if price <= 0:
+        raise MyException(price)
+except Exception as x:
+    print('Error,price value:', x, 'not correct')
+else:
+    print('Everything is fine')
+finally:
+    print('Verification done')
+
+
+
--- src/object/my_exception.py
+++ src/object/my_exception.py
@@ -1,12 +1,11 @@
 
+
 class MyException(Exception):
     '''атрибуты исключения'''
     def __init__(self, price):
         self.price = price
 
-    def __str__(self):
-        return f'\t {self.price} > 0  - Everything is fine\n'\
-               f'\t {self.price} <= 0  - You entered the wrong price value\n'
+
 
 
 
